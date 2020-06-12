babdeabe461182716245f26bba08a2efcb35ec50
Ahmed Adisa <adisa.ahmed881@gmail.com>
2020-06-12 09:15:45 +0100

    strutucring and some basic pages

---
+++ lets-talk.php
@@ -0,0 +1,277 @@
+<html>
+	<head>
+		<title>
+			Contact Us - Strategos 
+		</title>
+		<link rel="stylesheet" href="css/bootstrap_4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/font/font.css">
+		<link rel="stylesheet" href="css/font/font-awesome/css/font-awesome.min.css">
+	</head>
+
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-white navbar_navbar">
+		  	<a class="navbar-brand" href="#">
+				<img src="img/strategos-logo.png" alt="The Company Logo" class="company_logo">
+		  	</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		    	
+		    </ul>
+
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="our-company.html">Our Company</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="what-we-do.html">What We Do</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Blog</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link nav_link_padding" href="lets-talk.php">Let's talk</a>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
+
+		<div class="contact_banner">
+			<div class="home_banner_overlay"></div>
+			<div class="contact_banner_banner_caption">
+				<h1 class="banner_caption_h1">
+					Contact.
+				</h1>
+				<p class="banner_caption_p">
+					Our practice is built around the specific areas that we possess <br />
+					deep subject matter expertise as a result of our experience. <br />
+					Our practice is built around the specific areas that we possess <br />
+					deep subject matter expertise as a result of our experience.
+				</p>
+			</div>
+			<div class="banner_img">
+				<a href="lets-talk.php" class="lets_talk_button">
+					Let's Talk
+				</a>
+				<img src="img/strategos-logo-without-button.png" class="stratelogo" alt="">
+			</div>
+		</div>
+
+		<div class="sendmessage_section">
+			<div class="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<h1 class="sendmessage_section_h1">How can we help your business?</h1>
+							<span class="sendmessage_section_span"></span>
+							<p class="sendmessage_section_p">
+								Thank you for your interest in Strategos’s services. So we<br> can help serve you better, please take a moment to complete<br> this form and we will get in touch with you soon.
+							</p>
+
+							<form class="sendmessage_section_form">
+								<div class="form-group">
+									<label class="">Full Name</label><input name="name" type="text" class="form-control">
+								</div>
+								<div class="form-group">
+									<label class="">Company Email</label><input name="email" type="email" class="form-control">
+								</div>
+								<div class="form-group">
+									<label class="">Phone</label><input name="phone" type="text" class="form-control">
+								</div>
+								<div class="form-group">
+									<label class="">Organisation</label>
+									<input name="organisation" type="text" class="form-control">
+								</div>
+								<div class="form-group">
+									<label class="">Job Title</label>
+									<input name="title" type="text" class="form-control">
+								</div>
+								<div class="form-group">
+									<label class="">What service are you interested in?</label>
+									<select name="services" class="form-control">
+										<option>Investor Relations</option>
+										<option>Strategy &amp; Execution</option>
+										<option>Recruitment</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label class="">When can you be available for your consultation?</label>
+									<input name="consultation" type="date" class="form-control">
+								</div>
+								<div class="form-group">
+									<input class="form-check-input" id="" type="checkbox">
+									<label class="">I have read the Privacy Policy and agree to its terms.</label>
+								</div>
+								<p></p>
+								<div class="button">
+									<a href="" class="home_ourprocess_section_button_a">Send</a>
+								</div>
+							</form>
+						</div>
+						<div class="col-md-6">
+							<div class="parent">
+								<h1 class="contact_parent_h1">REACH US AT</h1>
+								<p class="contact_parent_p">
+									08090335255, 07018794787 
+									<br>
+									<b class="contact_parent_bbb">
+										32b Ramat Crescent, Ogudu GRA, Ogudu, 
+										<br>
+										Lagos, Nigeria
+									</b>
+								</p>
+								<div class="image">
+									<div class="image_banner_overlay"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="googlemap_section">
+			<div class="">
+				<div class="mapouter">
+					<div class="gmap_canvas">
+						<iframe width="100%" height="600" id="gmap_canvas" src="https://maps.google.com/maps?q=32b%20Ramat%20Crescent%2C%20Ogudu%20GRA%2C%20Ogudu%2C%20Lagos%2C%20Nigeria&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="STRATEGOS MAP"></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="news_letter_section">
+			<div class="background_img">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2">
+							<img src="img/strategos-newsletter-icon.png" class="news_letter_section_col_2_img">
+						</div>
+
+						<div class="col-md-10">
+							<h1 class="news_letter_section_col_10_h1">
+								Newsletter Sign Up
+							</h1>
+							<p class="news_letter_section_col_10_p">
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+							</p>
+
+							<form class="form-inline">
+								<div class="form-group mx-sm-3 mb-2 input">
+									<label for="inputPassword2" class="sr-only">First Name</label>
+									<input type="text" class="form-control" id="" placeholder="First Name">
+								</div>
+								<div class="form-group mx-sm-3 mb-2 input">
+									<label for="inputPassword2" class="sr-only">Last Name</label>
+									<input type="text" class="form-control" id="" placeholder="Last Name">
+								</div>
+								<div class="form-group mx-sm-3 mb-2 input">
+									<label for="inputPassword2" class="sr-only">Email Address</label>
+									<input type="text" class="form-control" id="" placeholder="Email Address">
+								</div>
+								<button type="submit" class="btn btn-primary mb-2 news_letter_section_button">Submit</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="footer_section">
+			<div class="background_img">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 footer_section_col_md_3">
+							<img src="img/strategos-logo.png" class="footer_section_col_md_3_img">
+							<p class="footer_section_col_md_3_p">
+								Phone: 08090335255, 07018794787 
+								<br>
+								Email: engage@strategoscompany.com
+								<br> 
+								<br>
+								Address: 32b Ramat Crescent, Ogudu GRA, Ogudu, Lagos, Nigeria
+							</p>
+						</div>
+						<div class="col-md-3 footer_section_col_md_3">
+							<h1 class="footer_section_col_md_3_h1">Service Offerings</h1>
+							<ul class="footer_section_col_md_3_ul">
+								<li class="footer_section_col_md_3_li">Performance Management</li>
+								<li class="footer_section_col_md_3_li">Business Model Improvement</li>
+								<li class="footer_section_col_md_3_li">Budgeting &amp; Financial Planning</li>
+								<li class="footer_section_col_md_3_li">Executive Search</li>
+								<li class="footer_section_col_md_3_li">Leadership Recruitment</li>
+								<li class="footer_section_col_md_3_li">Technology Recruitment</li>
+								<li class="footer_section_col_md_3_li">Executive support Recruitment</li>
+								<li class="footer_section_col_md_3_li">Investor Communications Strategy</li>
+								<li class="footer_section_col_md_3_li">Investor Presentation</li>
+								<li class="footer_section_col_md_3_li">Annual Report Writing</li>
+							</ul>
+						</div>
+						<div class="col-md-3 footer_section_col_md_3">
+							<h1 class="footer_section_col_md_3_h1">Navigations</h1>
+							<ul class="footer_section_col_md_3_ul">
+								<li class="footer_section_col_md_3_li">
+									<a href="index.html" class="footer_section_col_md_3_li_a">Home</a>
+								</li>
+								<li class="footer_section_col_md_3_li">
+									<a href="our-company.html" class="footer_section_col_md_3_li_a">Our Company</a>
+								</li>
+								<li class="footer_section_col_md_3_li">
+									<a href="what-we-do.html" class="footer_section_col_md_3_li_a">What We Do</a>
+								</li>
+								<li class="footer_section_col_md_3_li">
+									<a href="/" class="footer_section_col_md_3_li_a">Blog</a>
+								</li>
+								<li class="footer_section_col_md_3_li">
+									<a href="lets-talk.php" class="footer_section_col_md_3_li_a">Contact</a>
+								</li>
+							</ul>
+						</div>
+						<div class="col-md-3 footer_section_col_md_3">
+							<h1 class="footer_section_col_md_3_h1">Follow Us</h1>
+							<ul class="strategoscompany_media">
+								<li class="strategoscompany_media_li">
+									<a href=" https://www.instagram.com/thestrategoscompany_ng/" target="_blank">
+										<i class="fa fa-instagram"></i>
+									</a>
+								</li>
+								<li class="strategoscompany_media_li">
+									<a href="https://www.facebook.com/strategoscompanyNG/" target="_blank">
+										<i class="fa fa-facebook"></i>
+									</a>
+								</li>
+								<li class="strategoscompany_media_li">
+									<a href="https://www.linkedin.com/company/the-strategos-company-ng" target="_blank">
+										<i class="fa fa-linkedin"></i>
+									</a>
+								</li>
+								<li class="strategoscompany_media_li">
+									<a href="https://twitter.com/thestrategos_ng" target="_blank">
+										<i class="fa fa-twitter"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<p class="strategos_p">
+						©2019 Strategos | Privacy Policy
+						<b class="strategos_p_b">
+							<a target="_blank" href="http://onthegotechnologies.com/" class="strategos_p_a">Designed by OnTheGo Technologies</a>
+						</b>
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+</html>
